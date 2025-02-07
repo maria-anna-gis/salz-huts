079883fbef891f9f277ce653806ba02154806e16
Maria Anna <72383315+maria-anna-gis@users.noreply.github.com>
2025-02-07 18:42:26 +0100

    Add files via upload

---
+++ data/huts.js
@@ -0,0 +1,761 @@
+{
+   "type" "FeatureCollection",
+   "features" [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.351899,47.115744 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Albert-Biwak",
+    "category":"bivouac",
+    "elevation":2428,
+    "coords":"47.115744, 13.351899"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.1142,47.1272 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Gamsscharte ",
+    "category":"Shelter",
+    "elevation":2976,
+    "coords":"47.1272, 12.1142"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.775355,46.97939 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Dr.-Josef-Mehrl-Hütte",
+    "category":"3",
+    "elevation":1720,
+    "coords":"46.97939, 13.775355"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.042611,47.574593 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Carl-von-Stahl-Haus",
+    "category":"1",
+    "elevation":1736,
+    "coords":"47.574593, 13.042611"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.75035,47.47421667 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Passauer Hütte",
+    "category":"1",
+    "elevation":2033,
+    "coords":"47.47421667, 12.75035"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.08971667,47.0275 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Hagener Hütte",
+    "category":"1",
+    "elevation":2446,
+    "coords":"47.0275, 13.08971667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.67778333,47.29583333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Neue Bubenreuter Hütte",
+    "category":"1",
+    "elevation":950,
+    "coords":"47.29583333, 12.67778333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.047937,47.461727 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Eckbert-Hütte",
+    "category":"1",
+    "elevation":1144,
+    "coords":"47.461727, 13.047937"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.11611667,47.46028333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Ostpreußenhütte",
+    "category":"1",
+    "elevation":1630,
+    "coords":"47.46028333, 13.11611667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.38321667,47.17276667 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Neue Thüringer Hütte",
+    "category":"1",
+    "elevation":2212,
+    "coords":"47.17276667, 12.38321667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.42445,47.16528333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Neue Fürther Hütte",
+    "category":"1",
+    "elevation":2201,
+    "coords":"47.16528333, 12.42445"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.378154,47.148372 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Stickler-Hütte",
+    "category":"1",
+    "elevation":1750,
+    "coords":"47.148372, 13.378154"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.29261667,47.46958333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Freilassinger Hütte",
+    "category":"1",
+    "elevation":1500,
+    "coords":"47.46958333, 13.29261667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.726095,47.270979 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Landawirsee-Hütte",
+    "category":"1",
+    "elevation":1985,
+    "coords":"47.270979, 13.726095"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.272321,47.093937 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Ali-Lanti-Biwak",
+    "category":"bivouac",
+    "elevation":2663,
+    "coords":"47.093937, 13.272321"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.411955,47.100931 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Rotgüldensee-Hütte",
+    "category":"1",
+    "elevation":1740,
+    "coords":"47.100931, 13.411955"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.364414,47.198622 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Franz-Fischer-Hütte",
+    "category":"1",
+    "elevation":2020,
+    "coords":"47.198622, 13.364414"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.875376,47.463609 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Peter-Wiechenthaler-Hütte",
+    "category":"1",
+    "elevation":1752,
+    "coords":"47.463609, 12.875376"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.715296,47.422239 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Finsterbach-Hütte",
+    "category":"youth hostel",
+    "elevation":1420,
+    "coords":"47.422239, 12.715296"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.478186,47.505499 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Theodor-Körner-Hütte",
+    "category":"1",
+    "elevation":1456,
+    "coords":"47.505499, 13.478186"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.64628333,47.55063333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"von-Schmidt-Zabierow-Hütte",
+    "category":"1",
+    "elevation":1966,
+    "coords":"47.55063333, 12.64628333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.158362,47.160955 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Gamskarkogel-Hütte (Badgasteiner Hütte)",
+    "category":"1",
+    "elevation":2465,
+    "coords":"47.160955, 13.158362"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.495581,47.157547 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"St. Pöltner Hütte",
+    "category":"1",
+    "elevation":2481,
+    "coords":"47.157547, 12.495581"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.69638333,47.21195 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Krefelder Hütte",
+    "category":"2",
+    "elevation":2295,
+    "coords":"47.21195, 12.69638333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.124339,47.162724 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Zittauer Hütte",
+    "category":"1",
+    "elevation":2328,
+    "coords":"47.162724, 12.124339"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.76888333,47.21833333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Gleiwitzer Hütte",
+    "category":"1",
+    "elevation":2176,
+    "coords":"47.21833333, 12.76888333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.290299,47.556539 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Roßberg-Hütte",
+    "category":"1",
+    "elevation":1000,
+    "coords":"47.556539, 13.290299"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.007127,47.724494 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Zeppezauer-Haus",
+    "category":"2",
+    "elevation":1663,
+    "coords":"47.724494, 13.007127"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.13388333,47.12416667 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Richterhütte",
+    "category":"1",
+    "elevation":2374,
+    "coords":"47.12416667, 12.13388333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.733301,47.137853 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Gruberscharten-Biwak",
+    "category":"bivouac",
+    "elevation":3100,
+    "coords":"47.137853, 12.733301"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.312081,47.812808 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Schober-Hütte (bei Fuschl)",
+    "category":"bivouac",
+    "elevation":1329,
+    "coords":"47.812808, 13.312081"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.91445,47.45805 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Riemannhaus",
+    "category":"1",
+    "elevation":2177,
+    "coords":"47.45805, 12.91445"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.33638333,47.52055 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Laufener Hütte",
+    "category":"1",
+    "elevation":1726,
+    "coords":"47.52055, 13.33638333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.06028333,47.14805 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Hamburger Skihütte",
+    "category":"2",
+    "elevation":1970,
+    "coords":"47.14805, 13.06028333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.465999,47.246092 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Südwiener Hütte",
+    "category":"1",
+    "elevation":1802,
+    "coords":"47.246092, 13.465999"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.567346,47.245758 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Wismeyer-Haus",
+    "category":"3",
+    "elevation":1670,
+    "coords":"47.245758, 13.567346"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.88721667,47.50166667 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Ingolstädter Haus",
+    "category":"1",
+    "elevation":2119,
+    "coords":"47.50166667, 12.88721667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.346703,47.733824 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Braunauer Hütte",
+    "category":"2",
+    "elevation":1210,
+    "coords":"47.733824, 13.346703"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.920662,47.080835 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Otto-Umlauft-Biwak",
+    "category":"bivouac",
+    "elevation":2987,
+    "coords":"47.080835, 12.920662"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.316172,47.184527 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Tappenkarsee-Hütte",
+    "category":"1",
+    "elevation":1820,
+    "coords":"47.184527, 13.316172"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.968956,47.048128 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Rojacher-Hütte",
+    "category":"1",
+    "elevation":2718,
+    "coords":"47.048128, 12.968956"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.01028333,47.06028333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Niedersachsenhaus",
+    "category":"1",
+    "elevation":2471,
+    "coords":"47.06028333, 13.01028333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.764297,47.135488 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Schwarzenberg-Hütte",
+    "category":"1",
+    "elevation":2267,
+    "coords":"47.135488, 12.764297"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.519134,47.482302 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Hofpürgl-Hütte",
+    "category":"1",
+    "elevation":1705,
+    "coords":"47.482302, 13.519134"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.299826,47.122686 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Kürsinger-Hütte",
+    "category":"1",
+    "elevation":2558,
+    "coords":"47.122686, 12.299826"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.56223333,47.24643333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"DAV-Haus Obertauern",
+    "category":"2",
+    "elevation":1735,
+    "coords":"47.24643333, 13.56223333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.07166667,47.60945 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Purtschellerhaus",
+    "category":"1",
+    "elevation":1694,
+    "coords":"47.60945, 13.07166667"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.251693,47.092987 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Warnsdorfer Hütte",
+    "category":"1",
+    "elevation":2324,
+    "coords":"47.092987, 12.251693"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.79343333,47.62545 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Alte Traunsteiner Hütte",
+    "category":"1",
+    "elevation":1580,
+    "coords":"47.62545, 12.79343333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.290318,47.484121 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Dr.-Heinrich-Hackel-Hütte",
+    "category":"1",
+    "elevation":1526,
+    "coords":"47.484121, 13.290318"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.73888333,47.16278333 ]
+    },
+    "properties": {
+    "organisation":"German Alpenverein",
+    "name":"Heinrich-Schwaiger-Haus",
+    "category":"1",
+    "elevation":2802,
+    "coords":"47.16278333, 12.73888333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.379269,47.525932 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Gwechenberg-Hütte",
+    "category":"1",
+    "elevation":1375,
+    "coords":"47.525932, 13.379269"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 13.039731,47.400462 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Erich-Hütte",
+    "category":"1",
+    "elevation":1540,
+    "coords":"47.400462, 13.039731"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 12.957261,47.054095 ]
+    },
+    "properties": {
+    "organisation":"Austrian Alpenverein",
+    "name":"Zittel-Haus",
+    "category":"1",
+    "elevation":3105,
+    "coords":"47.054095, 12.957261"
+    }
+  }
+]
+}
---
+++ plugins/leaflet.infobutton/leaflet.infoButton.js
@@ -0,0 +1,84 @@
+L.Control.InfoButton = L.Control.extend({
+    options: {
+        position: 'topleft',
+        title:'<h1></h1>',
+        linkTitle:'Info',
+        html: '<p> Explore the locations of huts and roughly plan your routes between huts. Utilise the layers in the top right corner of the screen to customise the data which is visible. Huts are grouped according to the following categories.</p><p> <b>Category 1:</b> Typically have simple furnishings and, in the case of staffed huts, simple food. They are found in areas of importance for mountaineering and are typically over one hours walk from the nearest base.</p><p> <b>Category 2:</b> In frequently visited areas with better facilities and food compared to Category 1. These are suitable for stays of several days and holidays, typically open all year round.</p><p> <b>Category 3: </b> Accessible mechanically, mainly intended for day visits with typically only a small number of overnight stays.</p><p> <b>Bivouac:</b>A basic prefabricated shelter. They are usually located in remote locations.</p><p><b>Shelter:</b> Basic structure, not intended for overnight stays.</p><p><b>Youth hostel: </b> A base for young people for outdoor activities.</p><p>To learn more about the Alpenverein, visit their website <b><a href="https://www.alpenverein.at">here!</a> Click outside this box to explore the map. </b></p>',
+        show: true
+    },
+
+    onAdd: function (map) {
+        this.container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+
+        this.link = L.DomUtil.create('a', 'leaflet-bar-part leaflet-info-button', this.container);
+        this.link.href = '#';
+        this.link.title = this.options.linkTitle;
+        this.link.innerHTML = "?";
+        L.DomEvent.on(this.link, 'click', this._click, this);
+
+
+        this.infoWindowContainer = L.DomUtil.create('div', 'leaflet-infoWindow-container', this._map._container);
+        this.infoWindowBlack = L.DomUtil.create('div', 'leaflet-infoWindow-black', this.infoWindowContainer);
+        this.infoWindow = L.DomUtil.create('div', 'leaflet-infoWindow', this.infoWindowContainer);
+
+        L.DomEvent.on(this.infoWindowContainer, 'click', this._click, this);
+        L.DomEvent.on(this.infoWindow, 'click', this._stopClick, this);
+        this.title = L.DomUtil.create('div', 'leaflet-title', this.infoWindow);
+        this.title.innerHTML = this.options.title;
+        this.content = L.DomUtil.create('div', 'leaflet-content', this.infoWindow);
+        this.content.innerHTML = this.options.html;
+        L.DomEvent.disableClickPropagation(this.infoWindow);
+        L.DomEvent.disableClickPropagation(this.infoWindow);
+        if (this.options.show) this._showInfo();
+        return this.container;
+    },
+    _stopClick: function(e) {
+        L.DomEvent.stopPropagation(e);
+    },
+
+    _click: function (e) {
+        if (this.options.show == true) {
+            this._hideInfo();
+        } else {
+            this._showInfo();
+        }
+    },
+    _showInfo: function () {
+        this.infoWindowContainer.style.display ="block";
+
+        this.infoWindowBlack.style.animation ="showInfoContainer 0.2s";
+        this.infoWindowBlack.style.webkitAnimationName ="showInfoContainer 0.2s";
+        this.infoWindowBlack.style.opacity ="1";
+
+        this.infoWindow.style.animation ="showInfo 0.5s";
+        this.infoWindow.style.webkitAnimationName ="showInfo 0.5s";
+        this.infoWindow.style.top ="10%";
+        this.options.show = true;
+        this._map.dragging.disable();
+        this._map.touchZoom.disable();
+        this._map.doubleClickZoom.disable();
+        this._map.scrollWheelZoom.disable();
+    },
+    _hideInfo: function () {
+        this.infoWindowBlack.style.animation ="hideInfoContainer 0.2s";
+        this.infoWindowBlack.style.webkitAnimationName ="hideInfoContainer 0.2s";
+        this.infoWindowBlack.style.opacity ="0";
+
+        this.infoWindow.style.animation ="hideInfo 0.5s";
+        this.infoWindow.style.webkitAnimationName ="hideInfo 0.5s";
+        this.infoWindow.style.top ="-100%";
+        
+        var _this =this;
+        setTimeout(function (){_this.infoWindowContainer.style.display ="none";},500);
+        this.options.show = false;
+        this._map.dragging.enable();
+        this._map.touchZoom.enable();
+        this._map.doubleClickZoom.enable();
+        this._map.scrollWheelZoom.enable();
+    }
+});
+
+L.control.infoButton = function(options) {
+  var newControl = new L.Control.InfoButton(options);
+  return newControl;
+};
